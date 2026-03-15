836985d7b112d9604a13704613f30569881750a9
Maria GT <233059349+mariagt02@users.noreply.github.com>
2026-03-15 18:28:38 +0100

    models
    Co-Authored-By: Andreu <72315745+AndreuG01@users.noreply.github.com>

--- Assignments/Assignment 1/A1.py
+++ Assignments/Assignment 1/A1.py
@@ -113,11 +113,13 @@ def plot_degree_ccdf(degrees: list[int], out_path: str, savefig: bool=True, show
         plt.show()
 
 
-def characterize_network(net_name: str, data_path: str=DATA_PATH, out_path: str=OUT_PATH):
-    
-    
+def load_network(net_name: str, data_path: str=DATA_PATH) -> nx.Graph:
     net_data = nx.read_pajek(os.path.join(data_path, net_name))
     G = nx.Graph(net_data)
+    return G
+
+
+def characterize_network(G: nx.Graph, net_name: str, out_path: str=OUT_PATH):
     
     print(f"\n############ Characterization of {net_name} ############")
     num_nodes = G.number_of_nodes()
@@ -153,33 +155,123 @@ def characterize_network(net_name: str, data_path: str=DATA_PATH, out_path: str=
     
     print(f"Average shortest path length: {round(nx.average_shortest_path_length(G), 4)}")
     print(f"Diameter: {nx.diameter(G)}")
+
+# characterize_network(load_network("net1.net"))
+# characterize_network(load_network("net2.net"))
+# characterize_network(load_network("net3.net"))
+# characterize_network(load_network("net4.net"))
+
+
+# clustering_coefs_list = []
+# aspl = []
+# probs = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+# for p in probs:
+#     print(p)
+#     net1_fake = nx.watts_strogatz_graph(5000, 10, p)
+#     clustering_coefs = list(nx.clustering(net1_fake).values())
+#     mean_clustering = np.mean(clustering_coefs)
+#     mean_spl = nx.average_shortest_path_length(net1_fake)
     
-    
+#     aspl.append(mean_spl)
+#     clustering_coefs_list.append(mean_clustering)
+
+# fig = plt.figure(figsize=(6, 6))
+
+# plt.plot(probs, clustering_coefs_list, "o-", label="Clustering Coefficient")
+# plt.plot(probs, aspl, "s-", label="Average Shortest Path Length")
+
+# plt.xscale("log")
+
+# plt.savefig("ws_clustering_aspl.png", dpi=300)
+
+# net1_fake = nx.watts_strogatz_graph(5000, 10, 0.1)
+# characterize_network(net1_fake, net_name='net1_fake', out_path=OUT_PATH)
+
+# net2_fake = nx.erdos_renyi_graph(5000, 0.0021)
+# characterize_network(net2_fake, net_name='net2_fake', out_path=OUT_PATH)
+
+# net4 = load_network("net4.net")
+
+# net3 = load_network("net3.net")
+
+# net3_fake = nx.configuration_model([degree for _, degree in net3.degree()], create_using=None)
+# characterize_network(net3_fake, net_name='net3_fake', out_path=OUT_PATH)
+# degrees = list(dict(net3_fake.degree()).values())
+# sorted_deg = np.array(sorted(degrees))
+# k_vals = list(set(degrees))
+# # CCDF: P(K >= k)
+# ccdf = [(sorted_deg >= k).sum() / len(sorted_deg) for k in k_vals]
+
+# fig = plt.plot(np.log(k_vals), np.log(ccdf), "o", color="blue")
+# # plt.xscale("log")
+# # plt.yscale("log")
+
+# m, b = np.polyfit(np.log(k_vals), np.log(ccdf), 1)
+# # ln(y) = m*ln(x) + b -> y = exp(m*ln(x) + b) = x^m * exp(b)
+# # m =  - (gamma - 1)
+
+# gamma = 1 - m
+# y = k_vals ** m * np.exp(b)
+# plt.plot(np.log(k_vals), m * np.log(k_vals) + b, label=f"Gamma: {gamma}", color="black")
+# plt.legend()
+# plt.savefig("net3_fake_ccdf.png", dpi=300)
 
 
-characterize_network("net1.net")
-characterize_network("net2.net")
-characterize_network("net3.net")
-characterize_network("net4.net")
+# Visualize the network
+
+net5 = nx.read_pajek(os.path.join(DATA_PATH, "net5.net"))
+net5 = nx.Graph(net5)
+
+with open(os.path.join(DATA_PATH, "positions_net5.txt"), "r") as f:
+    positions = [line for line in f.read().split("\n")]
+
+positions = {
+    line.split("\t")[0]: [float(line.split("\t")[1]), float(line.split("\t")[2])] for line in positions[1:-1]
+}
 
 
+fig = plt.figure(figsize=(10, 5))
 
-# G = nx.read_pajek(os.path.join(DATA_PATH, "net5.net"))
-# G = nx.Graph(G)
-
-# with open(os.path.join(DATA_PATH, "positions_net5.txt"), "r") as f:
-#     positions = [line for line in f.read().split("\n")]
-
-# positions = {
-#     line.split("\t")[0]: [float(line.split("\t")[1]), float(line.split("\t")[2])] for line in positions[1:-1]
-# }
-
-
-# fig = plt.figure(figsize=(10, 5))
-
-# nx.draw_networkx_nodes(G, positions, node_size=10)
-# nx.draw_networkx_edges(G, positions, alpha=0.4)
-# # nx.draw_networkx_labels(G, positions, font_size=8)
+nx.draw_networkx_nodes(net5, positions, node_size=10)
+nx.draw_networkx_edges(net5, positions, alpha=0.4)
+# nx.draw_networkx_labels(G, positions, font_size=8)
 
 # plt.show()
 
+# Is the network connected?
+# No
+print(f"Network connected: {nx.is_connected(net5)}")
+
+
+# Is it scale-free (follows a power-law degree distribution)?
+net_name = "net5"
+degrees_net5 = list(dict(net5.degree()).values())
+
+plot_degree_hist_wrong(degrees_net5, out_path=os.path.join(OUT_PATH, f"{net_name.replace('.', '')}_hist.png"), show_fig=False)
+plot_degree_hist_wrong(degrees_net5, out_path=os.path.join(OUT_PATH, f"{net_name.replace('.', '')}_hist_log.png"), log_scale=True, show_fig=False)
+plot_degree_hist_logbin(
+    degrees_net5,
+    out_path=os.path.join(OUT_PATH, f"{net_name.replace('.', '')}_hist_logbin.png"),
+    show_fig=False
+)
+
+plot_degree_ccdf(
+    degrees_net5,
+    out_path=os.path.join(OUT_PATH, f"{net_name.replace('.', '')}_ccdf.png"),
+    show_fig=False
+)
+# NOT A POWER LAW 
+
+# Is the largest connected component a small-world network (high clustering coefficent and low aspl)?
+connected_components = list(nx.connected_components(net5))
+print(f"Number of connected components: {len(connected_components)}")
+print(f"Size connected components: {[len(c) for c in connected_components]}")
+max_cc = max(connected_components, key=len)
+max_cc_graph = net5.subgraph(max_cc)
+
+print(f"Clustering coefficient of the largetst CC: {round(np.mean(list(nx.clustering(max_cc_graph).values())), 4)}")
+print(f"Average shortest path length of the largest CC: {round(nx.average_shortest_path_length(max_cc_graph), 4)}")
+
+# Propose algorithm
+
+# Random geometric graph
